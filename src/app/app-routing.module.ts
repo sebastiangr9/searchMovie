d8f3d115f6cb725a8b0c5d6b2b7face9c42e2a4a
dase4e <sebasmetal9@gmail.com>
2018-07-03 11:50:12 -0500

    this app search actors and movies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MoviesComponent } from './movies/movies.component';
+import { ActorsComponent } from './actors/actors.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path:"movieDetails/:id",
+  //   component: MovieDetailsComponent
+  // },
+  // ,
+  // {
+  //   path:'movies',
+  //   component: MoviesComponent
+  // },
+  // {
+  //   path:'actors',
+  //   component: ActorsComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
